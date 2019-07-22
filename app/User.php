d44968949ce6d1f5ace870b73a5bcd792ad4d0a5
techacademy.zekkey <techacademy.zekkey@gmail.com>
2019-07-22 05:50:49 +0000

    Microposts Completed

--- app/User.php
+++ app/User.php
@@ -32,6 +32,11 @@ class User extends Authenticatable
         return $this->hasMany(Micropost::class);
     }
     
+    public function followings()
+    {
+        return $this->belongsToMany(User::class, 'user_follow', 'user_id', 'follow_id')->withTimestamps();
+    }
+    
     public function followers()
     {
         return $this->belongsToMany(User::class, 'user_follow', 'follow_id', 'user_id')->withTimestamps();
---
+++ database/migrations/2019_07_22_143222_create_microposts_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMicropostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('microposts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned()->index();
+            $table->string('content');
+            $table->timestamps();
+
+            // 外部キー制約
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('microposts');
+    }
+}
